112b48ca740912accda50fae37a7bbcec2c75353
shambhavi <shambhavivijay9@gmail.com>
2024-01-23 20:13:51 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo.model.ts
@@ -0,0 +1,11 @@
+export interface Todo{
+    id: number;
+    title:string;
+    description: string;
+    subtasks:string[];
+}
+
+/* 1
+   workout
+   morning workout
+   have breakfast after workout.. */
